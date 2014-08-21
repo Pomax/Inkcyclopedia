24a5777f6195cba5e2dbe5ed5d5853e055b58e3d
Pomax <pomax@nihongoresources.com>
2014-08-21 13:25:58 -0700

    sliding panes

--- public/javascript/imgloader.js
+++ public/javascript/imgloader.js
@@ -1,36 +1,99 @@
+var main = document.querySelector("main");
+var swatches = document.querySelector(".swatch.pane");
+var previews = document.querySelector(".preview.pane");
+
+function switchPanes(e) {
+  var switchpanes;
+  if(typeof e !== "boolean") {
+    e.preventDefault();
+    e.stopPropagation();
+    switchpanes = main.classList.contains("switched");
+  } else {
+    switchpanes = e;
+  }
+
+  if(switchpanes) {
+    main.classList.remove("switched");
+  } else {
+    main.classList.add("switched");
+  }
+
+  return false;
+};
+
+swatches.addEventListener("click", switchPanes);
+previews.addEventListener("click", switchPanes);
+
 function setStyle(e, s) {
   Object.keys(s).forEach(function(t) {
     e.style[t] = s[t];
   });
 }
 
-function loadImage(url, rgb) {
-  var underlay = document.createElement("div");
-  setStyle(underlay, {
-    background: "rgba(0,0,0,0.4)",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    height: "100%",
-    width: "100%",
-    textAlign: "center",
-    lineHeight: window.innerHeight + "px"
-  });
-  document.body.appendChild(underlay);      
-	var img = new Image();
-  img.src="ajax-loader.gif";
-  underlay.appendChild(img);
+var loaded = {};
 
-  var sample = new Image();
-  sample.onload = function() {
-    setStyle(sample, {
-      border: "10px solid " + rgb,
-      maxWidth: "calc(100% - 20px)"
+function loadImage(li, url, rgb) {
+
+  if(!loaded[url]) {
+
+    li.classList.add("previewing");
+    setStyle(li, {
+      borderColor: rgb,
+      background: rgb
     });
-    img.parentNode.replaceChild(sample,img);
-  };
-  sample.src = url;
-  underlay.onclick = function() {
-    underlay.parentNode.removeChild(underlay);
+
+    var sample = new Image();
+    sample.setAttribute("title", "click to remove");
+    sample.src = url;
+    setStyle(sample, {
+      boxSizing: "border-box",
+      border: "2px solid " + rgb
+    });
+
+    if(previews.children.length === 1) {
+      previews.querySelector("h1").classList.add("hidden");
+    }
+
+    sample.addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      li.classList.remove("previewing");
+      setStyle(li, {
+        borderColor: "#DDD",
+        background: "#DDD"
+      });
+
+      previews.removeChild(sample);
+      loaded[url] = false;
+
+      if(previews.children.length === 1) {
+        switchPanes(true);
+        previews.querySelector("h1").classList.remove("hidden");
+      }  
+
+      return false;
+    });
+
+    previews.appendChild(sample);
+    switchPanes(true);
+    loaded[url] = sample;
   }
+
+  else {
+
+    li.classList.remove("previewing");
+    setStyle(li, {
+      borderColor: "#DDD",
+      background: "#DDD"
+    });
+
+    previews.removeChild(loaded[url]);
+    switchPanes(false);
+    loaded[url] = false; 
+
+    if(previews.children.length === 1) {
+      previews.querySelector("h1").classList.remove("hidden");
+    }  
+  }
+
 }
